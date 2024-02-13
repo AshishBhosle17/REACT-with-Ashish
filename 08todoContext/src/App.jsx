e0438b62c7d2de68ee83144aac70c9fa14734efe
Ashish Bhosle <140106733+AshishBhosle17@users.noreply.github.com>
2024-02-13 08:59:08 -0800

    Add files via upload

--- 08todoContext/src/App.jsx
+++ 08todoContext/src/App.jsx
@@ -1,9 +1,47 @@
 import { useState } from 'react'
 import { TodoProvider } from './contexts'
 import './App.css'
+import React, { useEffect } from 'react';
+import TodoForm from './components/TodoForm'; // Adjust the path if necessary
+import TodoItem from './components/TodoItem'; // Adjust the path if necessary
+
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
 
